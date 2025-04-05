bd7c05e7c320dc4d4644308f16df9b4e57efacfd
Eble <markus.eble@sap.com>
2025-04-05 14:39:25 +0200

    StateMachine

---
+++ src/test/java/com/ABMachineTest.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.junit.Test;
+
+public class ABMachineTest extends MachineTestBase {
+
+    private void testWord(String word, Boolean expectOk) throws Exception {
+        testWord(new com.compiler.machines.ABMachine(), word, expectOk);
+    }
+
+    @Test
+    public void testAccept() throws Exception {
+        testWord("AB", true);
+        testWord("AABBBB", true);
+        testWord("ABB", true);
+        testWord("B", true);
+    }
+
+    @Test
+    public void testNoAccept() throws Exception {
+        testWord("BBBBAA", false);
+        testWord("A", false);
+    }
+}
